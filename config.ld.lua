0a2cfbfea8a7f02b278dfb8c439e37f071835fd9
russellhaley <russ.haley@gmail.com>
2017-06-17 22:05:31 -0700

    Synchronizing.
    Minor update

---
+++ config.ld.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: russellh
+-- Date: 5/29/17
+-- Time: 9:51 PM
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ scratch.lua
@@ -0,0 +1,14 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: russellh
+-- Date: 6/17/17
+-- Time: 9:55 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+local one = nil
+
+local two = 15
+
+print (one or two)
+
